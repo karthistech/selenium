60c8cde8c1074c7e23cc4a0bfa561655847e6556
karthistech <33998729+karthistech@users.noreply.github.com>
2020-09-24 21:19:22 +0530

    Creating Master Branch

---
+++ src/test/java/seleniumdemo/selenium/AppTest.java
@@ -0,0 +1,27 @@
+package seleniumdemo.selenium;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     * @throws InterruptedException 
+     */
+    @Test
+    public void shouldAnswerWithTrue() throws InterruptedException
+    {
+       System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+       WebDriver driver = new ChromeDriver();
+       driver.get("http://www.google.co.in");
+       Thread.sleep(5000);
+    }
+}
+ 
